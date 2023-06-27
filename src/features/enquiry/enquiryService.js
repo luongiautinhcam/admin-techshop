784e5d3eb64c96e2d79f97b240dc395b487fb634
Vinh <tranthanhvinh.info@gmail.com>
2023-06-27 17:48:18 +0700

    Enquiries

--- src/features/enquiry/enquiryService.js
+++ src/features/enquiry/enquiryService.js
@@ -1,13 +1,36 @@
 import axios from "axios";
+import { config } from "../../utils/axiosConfig";
 import { base_url } from "../../utils/base_url";
 
 const getEnquiries = async () => {
-  const response = await axios.get(`${base_url}enquiry/`);
+  const response = await axios.get(`${base_url}enquiries/`);
+  return response.data;
+};
+
+const updateEnquiry = async (enq) => {
+  const response = await axios.put(
+    `${base_url}enquiries/${enq.id}`,
+    { status: enq.enqData },
+    config
+  );
+  return response.data;
+};
+
+const getEnquiry = async (id) => {
+  const response = await axios.get(`${base_url}enquiries/${id}`, config);
+  return response.data;
+};
+
+const deleteEnquiry = async (id) => {
+  const response = await axios.delete(`${base_url}enquiries/${id}`, config);
   return response.data;
 };
 
 const enquiryService = {
   getEnquiries,
+  getEnquiry,
+  updateEnquiry,
+  deleteEnquiry,
 };
 
 export default enquiryService;
--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
-import { BiEdit } from "react-icons/bi";
-import { AiFillDelete } from "react-icons/ai";
+import { AiFillDelete, AiOutlineEye } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getEnquiries } from "../features/enquiry/enquirySlice";
+import {
+  deleteAEnquiry,
+  getEnquiries,
+  resetState,
+  updateAEnquiry,
+} from "../features/enquiry/enquirySlice";
+import CustomModal from "../components/CustomModal";
 
 const columns = [
   {
@@ -34,8 +39,18 @@ const columns = [
 ];
 
 const Enquiries = () => {
+  const [open, setOpen] = useState(false);
+  const [enqId, setenqId] = useState("");
+  const showModal = (e) => {
+    setOpen(true);
+    setenqId(e);
+  };
+  const hideModal = () => {
+    setOpen(false);
+  };
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(resetState());
     dispatch(getEnquiries());
   }, []);
   const enquiryState = useSelector((state) => state.enquiry.enquiries);
@@ -48,29 +63,68 @@ const Enquiries = () => {
       mobile: enquiryState[i].mobile,
       status: (
         <>
-          <select name="" className="form-control form-select" id="">
-            <option value="">Đặt trạng thái</option>
+          <select
+            name=""
+            defaultValue={
+              enquiryState[i].status ? enquiryState[i].status : "Da gui"
+            }
+            className="form-control form-select"
+            id=""
+            onChange={(e) =>
+              setEnquiryStatus(e.target.value, enquiryState[i]._id)
+            }
+          >
+            <option value="Da gui">Đã gửi</option>
+            <option value="Da lien he">Đã liên hệ</option>
+            <option value="Dang giai quyet">Đang giải quyết</option>
+            <option value="Da giai quyet">Đã giải quyết</option>
           </select>
         </>
       ),
       action: (
         <>
-          <Link className="fs-3 text-danger" to="/">
-            <BiEdit />
+          <Link
+            className="ms-3 fs-3 text-danger"
+            to={`/admin/enquiries/${enquiryState[i]._id}`}
+          >
+            <AiOutlineEye />
           </Link>
-          <Link className="fs-3 text-danger ms-3" to="/">
+          <button
+            className="fs-3 text-danger ms-3 bg-transparent border-0"
+            onClick={() => showModal(enquiryState[i]._id)}
+          >
             <AiFillDelete />
-          </Link>
+          </button>
         </>
       ),
     });
   }
+  const setEnquiryStatus = (e, i) => {
+    console.log(e, i);
+    const data = { id: i, enqData: e };
+    dispatch(updateAEnquiry(data));
+  };
+  const deleteEnq = (e) => {
+    dispatch(deleteAEnquiry(e));
+    setOpen(false);
+    setTimeout(() => {
+      dispatch(getEnquiries());
+    }, 100);
+  };
   return (
     <div>
       <h3 className="mb-4 title">Thắc mắc</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteEnq(enqId);
+        }}
+        title="Bạn có muốn xoá thắc mắc"
+      />
     </div>
   );
 };
