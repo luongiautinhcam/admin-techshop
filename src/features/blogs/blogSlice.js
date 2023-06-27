a399cf35e3a698924cf161cc3084eea92d63e1fa
Vinh <tranthanhvinh.info@gmail.com>
2023-06-27 20:07:35 +0700

    Add Blog

--- src/features/blogs/blogSlice.js
+++ src/features/blogs/blogSlice.js
@@ -1,5 +1,6 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
 import blogService from "./blogService";
+import { toast } from "react-toastify";
 
 export const getBlogs = createAsyncThunk("blog/get-blogs", async (thunkAPI) => {
   try {
@@ -8,6 +9,52 @@ export const getBlogs = createAsyncThunk("blog/get-blogs", async (thunkAPI) => {
     return thunkAPI.rejectWithValue(error);
   }
 });
+
+export const getABlog = createAsyncThunk(
+  "blog/get-blog",
+  async (id, thunkAPI) => {
+    try {
+      return await blogService.getBlog(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const createBlogs = createAsyncThunk(
+  "blog/create-blogs",
+  async (blogData, thunkAPI) => {
+    try {
+      return await blogService.createBlog(blogData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const updateABlog = createAsyncThunk(
+  "blog/update-blog",
+  async (blog, thunkAPI) => {
+    try {
+      return await blogService.updateBlog(blog);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteABlog = createAsyncThunk(
+  "blog/delete-blog",
+  async (id, thunkAPI) => {
+    try {
+      return await blogService.deleteBlog(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const resetState = createAction("Reset_all");
 
 const initialState = {
   blogs: [],
@@ -37,7 +84,75 @@ export const blogSlice = createSlice({
         state.isError = true;
         state.isSuccess = false;
         state.message = action.error;
-      });
+      })
+      .addCase(createBlogs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createBlogs.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdBlog = action.payload;
+        toast.success("Thêm bài viết thành công!");
+      })
+      .addCase(createBlogs.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        toast.error("Có lỗi gì đó!");
+      })
+      .addCase(getABlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getABlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.blogName = action.payload.title;
+        state.blogDecs = action.payload.decscription;
+        state.blogCategory = action.payload.category;
+        state.blogImages = action.payload.images;
+      })
+      .addCase(getABlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(updateABlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateABlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.updatedBlog = action.payload;
+        toast.success("Chỉnh sửa thành công!");
+      })
+      .addCase(updateABlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(deleteABlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteABlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedBlog = action.payload;
+        toast.success("Xoá thành công!");
+      })
+      .addCase(deleteABlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);;
   },
 });
 
--- src/pages/Addblog.js
+++ src/pages/Addblog.js
@@ -1,65 +1,168 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomInput from "../components/CustomInput";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { InboxOutlined } from "@ant-design/icons";
-import { message, Upload } from "antd";
-const { Dragger } = Upload;
-const props = {
-  name: "file",
-  multiple: true,
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-};
+import Dropzone from "react-dropzone";
+import { delImg, uploadImg } from "../features/upload/uploadSlice";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import { createBlogs, resetState } from "../features/blogs/blogSlice";
+import { getCategories } from "../features/bcategory/bcategorySlice";
 
+let schema = yup.object().shape({
+  title: yup.string().required("Tên sản phẩm không được để trống"),
+  decscription: yup.string().required("Mô tả không được để trống"),
+  category: yup.string().required("Danh mục không được để trống"),
+});
 const Addblog = () => {
-  const [desc, setDesc] = useState();
-  const handleDesc = (e) => {
-    setDesc(e);
-  };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [images, setImages] = useState();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
+  const imgState = useSelector((state) => state.upload.images);
+  const bCatState = useSelector((state) => state.bCategory.bCategories);
+  const blogState = useSelector((state) => state.blog.blogs);
+  const { isSuccess, isError, isLoading, createdBlog } = blogState;
+  // useEffect(() => {
+  //   if (isSuccess && createdBlog) {
+  //     toast.success("Thêm bài thành công!");
+  //   }
+  //   if (isError) {
+  //     toast.error("Có lỗi gì đó!");
+  //   }
+  // }, [isSuccess, isError, isLoading]);
+
+  const img = [];
+  imgState.forEach((i) => {
+    img.push({
+      public_id: i.public_id,
+      url: i.url,
+    });
+  });
+
+  useEffect(() => {
+    formik.values.images = img;
+  }, [img]);
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      decscription: "",
+      category: "",
+      images: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createBlogs(values));
+      formik.resetForm();
+      setTimeout(() => {
+        dispatch(resetState());
+        navigate("/admin/blog-list");
+      }, 300);
+    },
+    // onSubmit: (values) => {
+    //   if (getBlogId !== undefined) {
+    //     const data = { id: getBlogId, blogData: values };
+    //     dispatch(updateABlog(data));
+    //     setTimeout(() => {
+    //       dispatch(resetState());
+    //       navigate("/admin/blog-list");
+    //     }, 100);
+    //   } else {
+    //     dispatch(createBlogs(values));
+    //     formik.resetForm();
+    //     setTimeout(() => {
+    //       dispatch(resetState());
+    //       navigate("/admin/blog-list");
+    //     }, 300);
+    //   }
+    // },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Thêm bài viết</h3>
       <div className="">
-        <form action="">
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from
-              uploading company data or other banned files.
-            </p>
-          </Dragger>
+        <form action="" onSubmit={formik.handleSubmit}>
           <div className="mt-4">
-            <CustomInput type="text" label="Tiêu đề bài viết" />
+            <CustomInput
+              type="text"
+              label="Tiêu đề bài viết"
+              name="title"
+              onChng={formik.handleChange("title")}
+              onBlr={formik.handleBlur("title")}
+              val={formik.values.title}
+            />
           </div>
-          <select name="" className="form-control py-3 mb-3" id="">
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
+          <select
+            name="category"
+            onChange={formik.handleChange("category")}
+            onBlur={formik.handleBlur("category")}
+            value={formik.values.category}
+            className="form-control py-3 mt-3"
+            id=""
+          >
             <option value="">Chọn danh mục bài viết</option>
+            {bCatState?.map((i, j) => {
+              return (
+                <option key={j} value={i.title}>
+                  {i.title}
+                </option>
+              );
+            })}
           </select>
+          <div className="error">
+            {formik.touched.category && formik.errors.category}
+          </div>
           <ReactQuill
             theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
+            className="mt-3"
+            name="decscription"
+            onChange={formik.handleChange("decscription")}
+            value={formik.values.decscription}
           />
+          <div className="error">
+            {formik.touched.decscription && formik.errors.decscription}
+          </div>
+          <div className="bg-white border-1  p-5 text-center mt-3">
+            <Dropzone
+              onDrop={(acceptedFiles) => dispatch(uploadImg(acceptedFiles))}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <p>
+                      Drag 'n' drop some files here, or click to select files
+                    </p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+          </div>
+          <div className="showimages d-flex flex-wrap mt-3 gap-3">
+            {imgState?.map((i, j) => {
+              return (
+                <div className="position-relative" key={j}>
+                  <button
+                    type="button"
+                    onClick={() => dispatch(delImg(i.public_id))}
+                    className="btn-close position-absolute"
+                    style={{ top: "10px", right: "10px" }}
+                  ></button>
+                  <img src={i.url} alt="" width={200} height={200} />
+                </div>
+              );
+            })}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
